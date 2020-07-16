e5b381bbe24dbef47884893e931f13aa6af84622
Eduardo <du_du.mba@hotmail.com>
2020-07-16 22:25:08 +0100

    commit

---
+++ src/AppStyled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #ff0000;
+  height: 100vh;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  background-color: #136713;
+  width: 80px;
+  flex-direction: column;
+`;
+
+export const Pagebody = styled.div`
+  display: flex;
+  background-color: #0000ff;
+  flex:1;
+`;
